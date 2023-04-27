839c2a328e449bcebe6f74505401ac9538846611
XverLxrd <tur-2002@mail.ru>
2023-04-27 12:16:27 +0700

    ReDisign registration page

--- chat-app/src/Components/Pages/Registration page/registration.jsx
+++ chat-app/src/Components/Pages/Registration page/registration.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './registration.css';
 import logo from '../../../image/Logo.png'
-import theam from '../../../image/theam.png'
+import them from '../../../image/theam.png'
 import MyInput from "../../UI/inputs/myInput";
 import MySelect from "../../UI/selectList/mySelect";
 import MyButton from "../../UI/btns/myButton";
@@ -9,35 +9,37 @@ import {Link} from "react-router-dom";
 
 function Registration(props) {
     return (
-        <div className={'reg__container'}>
-            <div className={'reg__content'}>
-                <img className={'reg__logo'} alt={'Logo'} src={logo}/>
+        <div className={'reg'}>
+            <div className={'reg__container'}>
+                <div className={'reg__content'}>
+                    <img className={'reg__logo'} alt={'Logo'} src={logo}/>
 
-                <div className={'reg__text'}>
-                    <h1 className={'reg__text_title'}>wellcome to</h1>
-                    <h1 className={'reg__text_name'}>chat</h1>
+                    <div className={'reg__text'}>
+                        <h1 className={'reg__text_title'}>welcome to</h1>
+                        <h1 className={'reg__text_name'}>chat</h1>
+                    </div>
+
+                    <div className={'reg__inputs'}>
+                        <MyInput type={'text'} title={'user name'}/>
+                        <MyInput type={'email'} title={'email'}/>
+                        <MySelect title={'gender'}/>
+                        <MyInput type={'date'} title={'date of birthday'}/>
+                        <MyInput type={'tel'} title={'phone'}/>
+                    </div>
+
+                    <div className={'reg__btns'}>
+                        <Link to={'/'}>
+                            <MyButton type={'submit'} text={'registration'}/>
+                        </Link>
+                        <Link to={'/'}>
+                            <MyButton text={'log In'}/>
+                        </Link>
+                    </div>
                 </div>
 
-                <div className={'reg__inputs'}>
-                    <MyInput/>
-                    <MyInput/>
-                    <MySelect/>
-                    <MyInput/>
-                    <MyInput/>
+                <div className={'reg__them'}>
+                    <img className={'reg__them_img'} alt={'BigLogo'} src={them}/>
                 </div>
-
-                <div className={'reg__btns'}>
-                    <Link to={'/'}>
-                        <MyButton/>
-                    </Link>
-                    <Link to={'/'}>
-                        <MyButton/>
-                    </Link>
-                </div>
-            </div>
-
-            <div className={'reg__theam'}>
-                <img alt={'BigLogo'} src={theam}/>
             </div>
         </div>
     );
